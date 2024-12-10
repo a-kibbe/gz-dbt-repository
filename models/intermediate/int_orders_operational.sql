8918059f7459ba7c9f0429092da7e44fb2b1db9e
Aman <akibbe@hotmail.co.uk>
2024-12-10 17:03:46 +0000

    amended again

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -2,9 +2,13 @@ SELECT
     orders_id
     , date_date
     , revenue
+    , purchase_cost
     , ROUND((margin + shipping_fee - logcost - ship_cost),2) operational_margin
     , quantity
     , margin
+    , logcost
+    , ship_cost
+    , shipping_fee
 FROM
 {{ref("int_orders_margin")}} ord_marg
 LEFT JOIN
