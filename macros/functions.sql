051bc811782a3baf3bd04d7006438bea9cabfac2
Aman <akibbe@hotmail.co.uk>
2024-12-11 16:08:16 +0000

    added function for margin percent added margin percent to sales margin int view

--- macros/functions.sql
+++ macros/functions.sql
@@ -1,8 +1,5 @@
-{% macro margin_percent(revenue, purchase_cost) %}
+{% macro margin_percent(revenue, purchase_cost, dp=2) %}
     
-    ROUND(SAFE_DIVIDE((revenue - purchase_cost), revenue), 2) = margin_percent 
+    ROUND(SAFE_DIVIDE((revenue - purchase_cost), revenue), {{ dp }})
 
 {% endmacro %}
-
-
--- , IFNULL(dp
